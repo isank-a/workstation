c327f865bdabecb1088e0f4c77ec60620ca27c3f
Isank <github@isank.in>
2023-04-06 02:23:31 +0530

    add neovim configuration

---
+++ dotfiles/config/nvim/init.lua
@@ -0,0 +1,14 @@
+vim.opt.syntax = on
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.swapfile = false
+
+vim.opt.termguicolors = true
