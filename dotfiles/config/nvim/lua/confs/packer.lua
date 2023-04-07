3de860dbf819910f59798e34e0fafa31bb195180
Isank <github@isank.in>
2023-04-07 11:15:55 +0530

    install nvim-treesitter for neovim

--- dotfiles/config/nvim/lua/confs/packer.lua
+++ dotfiles/config/nvim/lua/confs/packer.lua
@@ -15,6 +15,11 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     -- or                          , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
