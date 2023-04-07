e85a54e87bbe8ab2437266378e5b0b24a6b22e84
Isank <github@isank.in>
2023-04-07 09:54:46 +0530

    install packer and telescope for neovim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,16 +1,2 @@
-vim.opt.guicursor = ""
-
-vim.opt.syntax = on
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.smartindent = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-
-vim.opt.swapfile = false
-
-vim.opt.scrolloff = 8
+require("confs.sets")
+require("confs.packer")
